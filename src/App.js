3f45727e7fdc2ec1f1e939d4ecbe6cbd13249488
mblanc2414 <mblanc2414@gmail.com>
2022-06-17 13:26:58 -0400

    Routes Added/ Login in progress

--- src/App.js
+++ src/App.js
@@ -3,23 +3,43 @@ import Landing from './pages/Landing'
 import Dashboard from './pages/Dashboard'
 import Category from './pages/Category'
 import {Route} from 'react-router'
-
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Snippets from './pages/Snippets'
+import Todo from './pages/Todo'
 function App() {
   return (
     <>
       {/* Ternary conditional to render landing if user is not logged in on first page load user? route dashboard : route landing*/}
-      <Route path='/'> {/* potentially change URI to something else*/ }
+      <Route exact path='/'> {/* potentially change URI to something else*/ }
         <Landing />
       </Route>
       <Route path='/dashboard'>
         <Dashboard />
       </Route>
       <Route
-        path='/:category'
+        path='/category/:category'
         render={(renderProps) => (
           <Category {...renderProps} />
-        )}
-      />
+        
+        )} />
+        <Route
+        path='/login'>
+          <Login/>
+        </Route>
+        <Route
+        path='/signup'>
+          <Signup/>
+        </Route>
+        <Route
+        path='/snippets'> 
+          <Snippets/>
+        </Route>
+        <Route
+        path='/todo'>
+          <Todo/>
+        </Route>
+        
     </>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,23 @@ import NavBar from '../components/NavBar'
 const Login = (props) => {
   return (
     <div>Login</div>
+    
   )
 }
 
 export default Login
+
+// import NavBar from '../components/NavBar'
+// import SignUpForm from '../components/SignUpForm'
+
+// const Signup = (props) => {
+//   return (
+//     <>
+//       <NavBar />
+//       <SignUpForm />
+//       <button>Sign in with Google</button> {/*onclick(signUpGoogle) */ }
+//     </>
+//   )
+// }
+
+// export default Signup
