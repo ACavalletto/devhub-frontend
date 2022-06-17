32898f0d3ed19f2e89861086e18cf742030ab559
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-17 08:27:49 -0600

    added routing to index.js, app.js as well as Links in sidebar

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import Dashboard from './pages/Dashboard'
 function App() {
   return (
     <>
-      {/* Ternary conditional to render landing if user is not logged in on first page load */}
-      <Landing />
-      <Dashboard />
+      {/* Ternary conditional to render landing if user is not logged in on first page load user? route dashboard : route landing*/}
+      <Route path='/welcome'> {/* potentially change URI to something else*/ }
+        <Landing />
+      </Route>
+      <Route path='/dashboard'>
+        <Dashboard />
+      </Route>
     </>
   );
 }
