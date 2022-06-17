2695543a0f7dc596b298d64481da0689a36e922c
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-16 19:29:58 -0600

    built out basic navbar layout need to add ternary to conditionally render login/signup buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,24 @@
+import { Link } from 'react-router-dom'
 const NavBar = (props) => {
   return (
-    <div>NavBar</div>
+    <div className='nav-bar'>
+      <Link to='/'>
+        {/* Links to landing page */}
+        <div>DevHub</div>
+      </Link>
+
+      {/* Wrap these links in ternary to display one or the other depending on whether you're on sign up page or login page */}
+      <Link to='/login'>
+        {/* Links to sign in page */}
+        <div>Login</div>
+      </Link>
+      <Link to='/signup'>
+        {/* Links to sign up page */}
+        <div>Sign up</div>
+      </Link>
+
+
+    </div>
   )
 }
 
