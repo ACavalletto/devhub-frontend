29a20a6ef9eca00afe55481a6424ab9a2465f574
mblanc2414 <mblanc2414@gmail.com>
2022-06-17 13:50:45 -0400

    Edited/Added Forms/Updated File Structure

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,12 @@
 import NavBar from '../components/NavBar'
-
+import LoginForm from '../components/forms/LoginForm'
 const Login = (props) => {
   return (
-    <div>Login</div>
-    
+    <div><LoginForm/>
+
+</div>
+// login onclick inside forms
   )
 }
 
 export default Login
-
-// import NavBar from '../components/NavBar'
-// import SignUpForm from '../components/SignUpForm'
-
-// const Signup = (props) => {
-//   return (
-//     <>
-//       <NavBar />
-//       <SignUpForm />
-//       <button>Sign in with Google</button> {/*onclick(signUpGoogle) */ }
-//     </>
-//   )
-// }
-
-// export default Signup
