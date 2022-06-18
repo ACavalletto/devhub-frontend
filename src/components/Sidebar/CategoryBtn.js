ecfa1a6eeb5403dc14e34088cabb5a4121a452c4
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-18 16:26:58 -0600

    added collapsible functionality to category-btn

--- src/components/Sidebar/CategoryBtn.js
+++ src/components/Sidebar/CategoryBtn.js
@@ -1,12 +1,21 @@
 import { Link } from 'react-router-dom'
 import category from '../../assets/category.svg'
 
+const collapsible = (e) => { 
+  let target = e.target.parentElement;
+  let links = target.nextSibling;
+  console.log(links)
+  links.style.display === 'block' ? links.style.display = 'none': links.style.display='block'
+
+}
+
 const CategoryBtn = (props) => {
   // Collapse/Expand functionality built here
   return (
     <div className='categories'>
-      <button className='category-btn'><img src={category}/></button>
+      <button className='category-btn' onClick={collapsible}><img src={category}/></button>
       {/* the URI's are placeholders for now */}
+      <div className="category-links" style={{ display: 'none'}}>
       <Link to='/categorya'>
         <div>Category A</div>
       </Link>
@@ -15,7 +24,8 @@ const CategoryBtn = (props) => {
       </Link>
       <Link to='/categoryc'>
         <div>Category C</div>
-      </Link>
+        </Link>
+      </div>
     </div>
   )
 }
