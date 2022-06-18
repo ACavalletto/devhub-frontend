3f84566656bb66b612b6ccc0baf8833764cde297
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-18 17:44:55 -0600

    added github and linkedin svgs and put them onto header bar

--- src/components/HeaderBar/Header.js
+++ src/components/HeaderBar/Header.js
@@ -1,8 +1,21 @@
 import { Link } from 'react-router-dom'
+import '../../style/header.css'
+import github from '../../assets/github.svg'
+import linkedin from '../../assets/linkedin.svg'
 
 const Header = (props) => {
   return (
-    <div style={{height: '5vh', background: '#00193b'}}>Header</div>
+    <div className='header'>
+      <div className='site-name'>
+        <h4>DevHub</h4>
+      </div>
+      <div className='links'>
+        <Link to='github.com'>
+          <img src={github} alt='github icon' />
+          <img src={linkedin} alt='linkedin icon' />
+        </Link>
+      </div>
+    </div>
   )
 }
 
