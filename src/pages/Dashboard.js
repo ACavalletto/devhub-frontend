95fc0246f1fa4a44ae740219d20db2cbd3f27471
mblanc2414 <mblanc2414@gmail.com>
2022-06-23 10:24:42 -0400

    card display

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import Sidebar from '../components/Sidebar/Sidebar'
 import Header from '../components/HeaderBar/Header'
 import Category from './Category'
 import { Link } from 'react-router-dom'
-import style from '../style/dash.module.css' 
+import '../style/dash.module.css' 
 // check linking for main rectangle frame/ check css style importing between dash/category
 const Dashboard = (props) => {
   console.log(props.user)
