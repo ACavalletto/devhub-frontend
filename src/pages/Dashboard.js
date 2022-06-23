07b19167bb2fa23bcad95ec28c65c828c0f52ff8
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-23 12:56:24 -0600

    Category now shows content pulled from database. And can delete content.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,23 +4,34 @@ import Category from './Category'
 import { Link } from 'react-router-dom'
 import style from '../style/dash.module.css' 
 // check linking for main rectangle frame/ check css style importing between dash/category
-const Dashboard = (props) => {
+const Dashboard = ({ URL, user }) => {
+  
+  // const createContent = async (category,content) => { 
+  //   await fetch(URL + 'content/' + user.uid + category, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'Application/json'
+  //     },
+  //     body: JSON.stringify(content)
+  //   })
+  // }
+
   return (
     <div className={style.dashboard}>
       <div className={style.header_container}>
-      <Header user={props.user} />
+      <Header user={user} />
       </div>
       <div className={style.sidebar_container}>
-        <Sidebar user={props.user} />
+        <Sidebar user={user} />
       </div>
       {/* URI's are placeholders for now */}
       <div className={style.categories}>
-      <div className={style.category_container}>
-      <Link to='/categorya'>
-        <Category />
-        </Link>
-        </div>
         <div className={style.category_container}>
+        <h4>Category A</h4>
+           
+          <Category user={user.displayName} id={user.uid} URL={URL} category='a'/>
+      </div>
+      <div className={style.category_container}>
       <Link to='/categoryb'>
         <Category />
           </Link>
